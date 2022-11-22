b5aaf759c099a9a7e63dffa151881b1bbab908d9
AndresBaide <117405434+AndresBaide@users.noreply.github.com>
2022-11-22 17:17:35 -0600

    Add files via upload

--- empleados/models/models.py
+++ empleados/models/models.py
@@ -2,19 +2,20 @@
 
 from odoo import models, fields, api
 
-
+class departamentos(models.Model):
+     _name = 'empleados.departamentos'
+     _description = 'empleados.departamentos'
+     n_departamento = fields.Char(string="Nombre del departamento")
+     
+     
 class empleados(models.Model):
      _name = 'empleados.empleados'
      _description = 'empleados.empleados'
      name = fields.Char(string="Nombre")
      edad = fields.Integer(string="Edad")
      numero_tlf = fields.Char(string="Numero de telefono")
-     #n_departamento = fields.Many2one('empleados.departamentos',  string="Departamentos", required=True)
-     
-#class departamentos(models.Model):
-#     _name = 'empleados.departamentos'
-#     _description = 'empleados.departamentos'
-#     n_departamento = fields.Char(string="Nombre del departamento")
+     n_departamento = fields.Many2one('empleados.departamentos',  string="Departamentos")
+
      
      
      
